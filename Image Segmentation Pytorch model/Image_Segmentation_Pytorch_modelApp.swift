4bfa41f3899670aee34a07501d83f73936353e72
kaushik200030 <f20190653@pilani.bits-pilani.ac.in>
2024-02-25 03:48:09 +0530

    Initial Commit

---
+++ Image Segmentation Pytorch model/Image_Segmentation_Pytorch_modelApp.swift
@@ -0,0 +1,17 @@
+//
+//  Image_Segmentation_Pytorch_modelApp.swift
+//  Image Segmentation Pytorch model
+//
+//  Created by KAUSHIK PATTANAYAK on 25/02/24.
+//
+
+import SwiftUI
+
+@main
+struct Image_Segmentation_Pytorch_modelApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
